f88ffbecd17629f5bd8f5d278547200b0731196b
margeodev <margeodev@gmail.com>
2018-11-15 19:57:51 -0300

    Criação do projeto

---
+++ src/main/java/com/pessoal/api/VendasBasicoApiApplication.java
@@ -0,0 +1,12 @@
+package com.pessoal.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasBasicoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendasBasicoApiApplication.class, args);
+	}
+}
